58c5970909337ef28e26cf8fffa640b574fb590b
Alessio Signorini <alessio@signorini.us>
2020-01-14 10:25:41 -0800

    small improvement on data["original_id"]

--- lib/huginn_withings_agent/withings_agent.rb
+++ lib/huginn_withings_agent/withings_agent.rb
@@ -97,7 +97,7 @@ module Agents
         data['end_time']     ||= data['start_time'].to_i + data['duration'].to_i
         data['date']         ||= Time.at(data['start_time'].to_i).in_time_zone(data['timezone']).to_date.to_s
 
-        event_id    = data['original_id'] || event.id
+        event_id    = data['original_id'].blank? ? event.id : data['original_id']
         old_id      = memory.dig('seen', event_id)
         activity_id = push_activity(data, old_id)
           log('Failed to create activity', event) and next unless activity_id
